b3c1d50fa0156ac1ca6c79da5c9457f3f27f3be4
Matt Tierney <tierney@ubuntu.(none)>
2011-05-20 15:34:45 -0400

    able to extract overall distance. Now on to extracting step-by-step directions.

--- src/google-maps-directions/get-directions-distance.py
+++ src/google-maps-directions/get-directions-distance.py
@@ -2,29 +2,57 @@
 
 import urllib
 import urllib2
+from BeautifulSoup import BeautifulSoup
+from threading import Thread
 
-TIMEOUT=10 # seconds
+TIMEOUT = 10 # seconds
 
-class GoogleMaps(object):
-    def __init__(self):
-        pass
+class GoogleMaps(Thread):
+    def __init__(self, saddr, daddr):
+        Thread.__init__(self)
+        self.saddr = saddr
+        self.daddr = daddr
     
     def _fetch_data(self, saddr, daddr):
-        """
-        wget http://maps.google.com/maps?saddr={start address}&daddr={destination address}
-        http://maps.google.com/maps?saddr=Obuasi,%20Ghana&daddr=Accra,%20Ghana
-        """
         quoted_saddr = urllib.quote_plus(saddr)
         quoted_daddr = urllib.quote_plus(daddr)
         url = "http://maps.google.com/maps?saddr=%s&daddr=%s" % (quoted_saddr, quoted_daddr)
-        
         out = urllib2.urlopen(url, None, TIMEOUT)
-        return out.readlines()
-        
+        return (url, out.readlines())
+
+    def _parse_highlevel(self, soup):
+        out = soup.findAll("div", { "class" : "dir-altroute-inner" })
+        return out
+
+    def _parse_steps(self, soup):
+        count = -1
+        while True:
+            count += 1
+            out = soup.find("tbody", { "id" : "step_0_%d" % (count) })
+            if not out:
+                break
+            else:
+                print out
+
+    def run(self):
+        (url, out) = self._fetch_data(self.saddr, self.daddr)
+        html = "\n".join(out) # clean the html by de-listing it.
+        # Use BeautifulSoup to find the html with dist info.
+        soup = BeautifulSoup(html)
+
+        self.location_html = self._parse_highlevel(soup)
+        self._parse_steps(soup)
+
 def main():
-    g = GoogleMaps()
-    out = g._fetch_data("Accra, Ghana", "Madina, Ghana")
-    print out
-    
+    g = GoogleMaps("Accra, Ghana", "Madina, Ghana")
+    g.run()
+    # print g.location_html
+
+    for soup in g.location_html:
+        for div in soup.findAll("div")[:-1]:
+            print " ", div.contents
+            
+        # print soup.find("div", { "class" : "altroute-rcol altroute-info" })
+
 if __name__=="__main__":
     main()
