67f1585695a7a3eed62bff825b7a6784404bde7f
Matt Tierney <tierney@cs.nyu.edu>
2011-05-24 11:10:24 -0400

    Adding VillageParser

---
+++ src/google-maps-directions/VillageParser.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+class VillageParser(object):
+    def __init__(self, filename):
+        self.filename = filename
+        self.villages = list()
+        
+    def _decomment(self, village_lines):
+        # Remove comment lines
+        villages = list()
+        for line in village_lines:
+            # empty line
+            if not line:
+                continue
+
+            hash_pos = line.find("#")
+            if hash_pos == 0:
+                continue
+            elif hash_pos > 0:
+                villages.append(line[:hash_pos])
+            else:
+                villages.append(line)
+        return villages
+
+    def run(self):
+        with open(self.filename) as fh:
+            village_lines = [l.strip() for l in fh.readlines()]
+
+        self.villages = self._decomment(village_lines)
+
+def main():
+    vp = VillageParser("../../data/villages.txt")
+    vp.run()
+    villages = vp.villages
+
+    for i, vill in enumerate(villages):
+        print i, vill
+
+if __name__=="__main__":
+    main()
